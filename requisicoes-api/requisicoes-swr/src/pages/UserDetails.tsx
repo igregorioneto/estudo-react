aa373201b42f4d8c41a0f4851fab07bf4a6f44da
Gregório <igregoriodoneto@outlook.com>
2022-04-22 16:42:29 -0300

    Estrutura das pages da listagem de usuário e detalhes do usuário

--- requisicoes-api/requisicoes-swr/src/pages/UserDetails.tsx
+++ requisicoes-api/requisicoes-swr/src/pages/UserDetails.tsx
@@ -1,53 +1,26 @@
-import { userInfo } from 'os';
-import React, { useCallback } from 'react';
-import { Link } from 'react-router-dom';
-import { mutate as mutateGlobal } from 'swr';
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import { useFetch } from '../hooks/useFetch';
-import api from '../services/api';
 
 interface User {
     id: number;
     name: string;
 }
 
-const UserList: React.FC = () => {
-    const { data, mutate } = useFetch<User[]>('users');
-
-    const handleNameChange = useCallback((id: number) => {
-        api.put(`users/${id}`, { name: 'João' });
-
-        const updatedUsers = data?.map(user => {
-            if(user.id === id) {
-                return { ...user, name: 'João' }
-            }
-
-            return user;
-        });
-
-        mutate(updatedUsers, false);
-        mutateGlobal(`users/${id}`, { id, name: 'João' })
-        
-    }, [data, mutate]);
+const UserDetails: React.FC = () => {
+    const { id } = useParams();
+    const { data } = useFetch<User>(`users/${id}`);
 
     if(!data) {
         return <p>Carregando...</p>
     }
 
-
     return (
         <ul>
-            {data.map(user => (
-                <li key={user.id}>
-                    <Link to={`/users/${user.id}`}>
-                        {user.name}
-                    </Link>
-                    <button type='button' onClick={() => handleNameChange(user.id)}>
-                        Alterar nome
-                    </button>
-                </li>
-            ))}
+            <li>ID: {data?.id}</li>
+            <li>Name: {data?.name}</li>
         </ul>
     );
 }
 
-export default UserList;
+export default UserDetails;
