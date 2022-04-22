f2875242f7a938ef927304d755401c917c91bc5a
Gregório <igregoriodoneto@outlook.com>
2022-04-21 21:40:53 -0300

    Componente Funcional

---
+++ componentes-classe-e-funcional/componente-funcional/src/Avatar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const styles = {
+  root: {
+    margin: '5vw'
+  },
+  avatar: {
+    width: 100,
+    borderRadius: '50%',
+    padding: 5,
+    boxShadow: '0px 5px 25px 0px rgba(0,0,0,0.5)'
+  }
+}
+
+const Avatar = ({url}) => {
+  return (
+    <div style={styles.root}>
+      <img src={url} alt="Avatar" style={styles.avatar}/>
+    </div>
+  );
+}
+
+export default Avatar;
---
+++ componentes-classe-e-funcional/componente-funcional/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Avatar from './Avatar';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Avatar url="http://static.tumblr.com/u7rwwlh/nyNm6v1n7/tonyavatar.png"/>
+  </React.StrictMode>
+);
